55eef9222fae3eb8997e5a5985fcb0b0632e7c71
Diego cristino <Diegocristino77@gmail.com>
2026-05-17 15:42:53 -0300

    adicionando comentario

--- async_views/asyncviews/views.py
+++ async_views/asyncviews/views.py
@@ -2,7 +2,7 @@ import asyncio
 import httpx
 from django.http import HttpResponse
 
-
+# funcao assincrona que faz uma chamada HTTP e retorna o resultado
 async def http_call_async():
     for num in range(1, 6):
         await asyncio.sleep(1)
